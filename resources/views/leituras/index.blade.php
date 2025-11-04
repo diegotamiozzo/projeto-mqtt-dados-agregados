3a3ac18deb11b67facbd052f9af6707da0fa4d8f
Diego <dietamiozzo@gmail.com>
2025-11-04 11:15:02 -0300

    ajuste logo

--- resources/views/leituras/index.blade.php
+++ resources/views/leituras/index.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Monitoramento de Equipamentos</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="icon" href="{{ asset('images/icone.png') }}" type="image/png">
     <style>
         .table-responsive {
             max-height: 80vh;
